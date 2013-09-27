5d1284ffbba1edac197444ee31f75cb012cd9ce0
Susanna Huhtanen <susanna.huhtanen@basilicom.de>
2013-09-27 14:37:14 +0200

    gauges cleaned

--- gauges/core/app/js/view.js
+++ gauges/core/app/js/view.js
@@ -22,14 +22,8 @@ cannonerd.views.FirstPageView = Backbone.View.extend({
     },
     stop: function(event){
         event.preventDefault();
-        if(cannonerd.config.inLocalMode){
-            window.clearInterval(this.timeoutclearer);
-        }else{
-            if (this.watchID) {
-                navigator.compass.clearWatch(this.watchID);
-                this.watchID = null;
-            }
-        }
+        window.clearInterval(this.timeoutclearer);
+
 
     },
     initialize: function (options) {
@@ -50,7 +44,6 @@ cannonerd.views.FirstPageView = Backbone.View.extend({
         //to get instant feedback on button pushes. For the user it FEELS faster
         document.addEventListener("touchstart", function(){}, true);
         var compassread = 0;
-        if(cannonerd.config.inLocalMode){
             this.timeoutclearer= window.setInterval(function(){
                 compassread = Math.random() * 360;
                 console.log(compassread);
@@ -58,29 +51,6 @@ cannonerd.views.FirstPageView = Backbone.View.extend({
                     "-webkit-transform": "rotate("+compassread+"deg)"
                 });
             }, 1000);
-        }else{
-
-            // Update compass every 3 seconds
-            var options = { frequency: 1000 };
-            console.log(navigator.compass);
-            this.watchID= navigator.compass.watchHeading(onSuccess, onError, options);
-                console.log(this.watchID);
-            // onSuccess: Get the current heading
-            //
-            function onSuccess(heading) {
-                console.log('Heading: ' + heading.magneticHeading);
-                $(".compassrotator", this.el).css({
-                    "-webkit-transform": "rotate("+heading.magneticHeading+"deg)"
-                });
-            }
-
-            // onError: Failed to get the heading
-            //
-            function onError(compassError) {
-                console.log('Compass Error: ' + compassError.code);
-            }
-
-        }
 
         return this;
     }
